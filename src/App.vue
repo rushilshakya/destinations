faf4eb6c8eef7b5f1c1ecfb423fd950a4f088a1c
rushilshakya <rushil.shakya@gmail.com>
2021-03-08 18:02:00 +0545

    feat: vue cli solution

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="apps">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js Appet" />
+		<div v-for="location in locations" :key="location.desc">
+			<app-place :location="location">
+				<h2>{{ location.name }}</h2>
+			</app-place>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import appPlace from "./components/appPlace.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		HelloWorld,
+		appPlace,
+	},
+	data() {
+		return {
+			locations: [
+				{
+					name: "moscow",
+					img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg",
+					desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`,
+				},
+				{
+					name: "paris",
+					img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg",
+					desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+				},
+				{
+					name: "rome",
+					img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg",
+					desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`,
+				},
+				{
+					name: "paris",
+					img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg",
+					desc: `By the end of the 12th century, Paris had become the political, economic, religious, and cultural capital of France. Maurice de Sully undertook the construction of the Notre Dame Cathedral at its eastern extremity.`,
+				},
+			],
+		};
+	},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#apps {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
